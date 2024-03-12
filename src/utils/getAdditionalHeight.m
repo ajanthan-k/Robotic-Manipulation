19ed6e8afb7541c202fa0e3c1ea30e22eafea5c3
MITeo21 <meigan.teo21@imperial.ac.uk>
2024-03-12 14:04:20 +0000

    New function

--- src/utils/getAdditionalHeight.m
+++ src/utils/getAdditionalHeight.m
@@ -2,14 +2,17 @@ function additional_height = getAdditionalHeight(coords)
     % fix for shoulder (as distance from origin increase, starts
     % drooping from arm weight => affects z)
     % params found using desmos line fitting method detailed in report
-    m = 0.08;
-    c = 9.5;
+    distance_from_robot = sqrt(coords(1)^2+coords(2)^2);
+    z = coords(3);
+    m = 0.0023*(z^2)-0.0354*z+0.0325;
+    c = 0.022*(z^2)+0.322*z-0.1998;
     k = 8.6;
     thres = 9;
-    distance_from_robot = sqrt(coords(1)^2+coords(2)^2);
+    
     if(distance_from_robot < thres)
         additional_height = 0;
     else
-        additional_height = (distance_from_robot * m) - (c-k);
+        %additional_height = (-1) * ((m * distance_from_robot) + c);
+        additional_height = 0;
     end
 end
