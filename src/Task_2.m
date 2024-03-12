46696fd0b522c2ac69ccf732d2dfeec57cb7aca3
MITeo21 <meigan.teo21@imperial.ac.uk>
2024-03-12 02:54:31 +0000

    Tidy up additional height function

--- src/Task_2.m
+++ src/Task_2.m
@@ -197,6 +197,12 @@ function additional_height = get_additional_height(coords)
     % params found using desmos line fitting method detailed in report
     m = 0.08;
     c = 9.5;
-    k=8.6;
-    additional_height = (sqrt(coords(1)^2+coords(2)^2) * m) - (c-k);
+    k = 8.6;
+    thres = 9;
+    distance_from_robot = sqrt(coords(1)^2+coords(2)^2);
+    if(distance_from_robot < thres)
+        additional_height = 0;
+    else
+        additional_height = (distance_from_robot * m) - (c-k);
+    end
 end
