ed622998db6cf944c57b67508a592a6dd6a2a12c
MITeo21 <meigan.teo21@imperial.ac.uk>
2024-03-14 00:13:50 +0000

    Fix pen dropoff

--- src/Task_3.m
+++ src/Task_3.m
@@ -26,8 +26,9 @@ function out_commands = Task_3(task_ID, pen_placement, current_pose)
         pen_end_position = [pen_placement(1)-((pen_to_floor_dist+pen_length)*cosd(60)), 0, ((pen_to_floor_dist+pen_length)*sind(60)),-stand_angle];
         out_commands = pickup_pen(pen_end_position, tip_length, stand_angle, current_pose, open_state, closed_state);
     elseif(task_ID == "dropoff")
-        pen_cap_opening_position = [pen_placement(1)-((pen_to_floor_dist+cap_length)*cosd(60)), 0, ((pen_to_floor_dist+cap_length)*sind(60)),-stand_angle];
-        out_commands = dropoff_pen(pen_cap_opening_position, tip_length, stand_angle, current_pose, open_state);
+        %pen_cap_opening_position = [pen_placement(1)-((pen_to_floor_dist+cap_length)*cosd(60)), 0, ((pen_to_floor_dist+cap_length)*sind(60)),-stand_angle];
+        pen_end_position = [pen_placement(1)-((pen_to_floor_dist+pen_length)*cosd(60)), 0, ((pen_to_floor_dist+pen_length)*sind(60)),-stand_angle];
+        out_commands = dropoff_pen(pen_end_position, tip_length, stand_angle, current_pose, open_state);
     end
 end
 
@@ -52,20 +53,41 @@ function out_list = pickup_pen(pen_end, tip, angle, current_pose, open_state, cl
     out_list(end+1,:) = [0, 15, working_height, hovering_angle];
 end
 
-function out_list = dropoff_pen(cap_opening, tip, angle, current_pose, open_state)
+% function out_list = dropoff_pen(cap_opening, tip, angle, current_pose, open_state)
+%     out_list = [];
+%     hovering_angle = -45;
+%     cap_entry_leeway = 1;
+%     push_leeway = 0.5;
+%     dropoff_clearance = 6;
+%     working_height = cap_opening(3)+dropoff_clearance;
+%     % Move to working height and then to cap opening
+%     out_list(end+1,:) = [current_pose(1), current_pose(2), working_height, hovering_angle];
+%     out_list(end+1,:) = [cap_opening(1)-((tip+cap_entry_leeway)*cosd(angle)), cap_opening(2), cap_opening(3)+((tip+cap_entry_leeway)*sind(angle)), -angle];
+%     % Push and release
+%     out_list(end+1,:) = [cap_opening(1)+((tip+push_leeway)*cosd(angle)), cap_opening(2), cap_opening(3)-((tip+push_leeway)*sind(angle)), -angle];
+%     out_list(end+1,:) = ["gripper", open_state, 0, 0];
+%     % Reset
+%     out_list(end+1,:) = [cap_opening(1)-((tip+cap_entry_leeway)*cosd(angle)), cap_opening(2), cap_opening(3)+((tip+cap_entry_leeway)*sind(angle)), -angle];
+%     out_list(end+1,:) = [current_pose(1), current_pose(2), working_height, 0];
+% end
+
+function out_list = dropoff_pen(pen_end, tip, angle, current_pose, open_state)
     out_list = [];
     hovering_angle = -45;
     cap_entry_leeway = 1;
     push_leeway = 0.5;
-    dropoff_clearance = 6;
-    working_height = cap_opening(3)+dropoff_clearance;
-    % Move to working height and then to cap opening
+    dropoff_clearance = 7;
+    working_height = pen_end(3) + dropoff_clearance;
+    % Move to working height above pen cap
     out_list(end+1,:) = [current_pose(1), current_pose(2), working_height, hovering_angle];
-    out_list(end+1,:) = [cap_opening(1)-((tip+cap_entry_leeway)*cosd(angle)), cap_opening(2), cap_opening(3)+((tip+cap_entry_leeway)*sind(angle)), -angle];
+    out_list(end+1,:) = [pen_end(1)-((tip+2*cap_entry_leeway)*cosd(angle)), pen_end(2), working_height, -angle];
+    % Position pen tip
+    out_list(end+1,:) = [pen_end(1)-((tip+cap_entry_leeway)*cosd(angle)), pen_end(2), pen_end(3)+((tip+cap_entry_leeway)*sind(angle)), -angle];
     % Push and release
-    out_list(end+1,:) = [cap_opening(1)+((tip+push_leeway)*cosd(angle)), cap_opening(2), cap_opening(3)-((tip+push_leeway)*sind(angle)), -angle];
+    out_list(end+1,:) = [pen_end(1)+(push_leeway*cosd(angle)), pen_end(2), pen_end(3)+(push_leeway*sind(angle)), -angle];
     out_list(end+1,:) = ["gripper", open_state, 0, 0];
-    % Reset
-    out_list(end+1,:) = [cap_opening(1)-((tip+cap_entry_leeway)*cosd(angle)), cap_opening(2), cap_opening(3)+((tip+cap_entry_leeway)*sind(angle)), -angle];
-    out_list(end+1,:) = [current_pose(1), current_pose(2), working_height, 0];
+    % Retreat and return to position
+    out_list(end+1,:) = [pen_end(1)-((tip+cap_entry_leeway)*cosd(angle)), pen_end(2), pen_end(3)+((tip+cap_entry_leeway)*sind(angle)), -angle];
+    out_list(end+1,:) = [pen_end(1)-((tip+2*cap_entry_leeway)*cosd(angle)), pen_end(2), working_height, -angle];
+    out_list(end+1,:) = [current_pose];
 end
