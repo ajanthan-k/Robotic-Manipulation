bf8767a16afc7abf489ad8f2381d3e2f29d53e7a
MITeo21 <meigan.teo21@imperial.ac.uk>
2024-03-17 17:44:09 +0000

    Increase effectiveness of Task 4

--- src/Task_4.m
+++ src/Task_4.m
@@ -19,19 +19,24 @@ movePos([18, 0, 9, -90], port_num, 1.0, 3)
 openGripper(3, 0, port_num);
 pause(1);
 movePos([16, 0, 20, -30], port_num, 1.0, 3);
+movePos([14, 0, 24, -30], port_num, 1.0, 3);
 
 %% box to jug
 setVAProfile(2000, 200, port_num); % big movement across workspace - slow down
+movePos([-4, 4, 24, -30], port_num, 1.0, 3);
 movePos([-15, 15, 20, -30], port_num, 2, 3);
 movePos([-19, 19, 21, -30], port_num, 1.0, 3);
 setVAProfile(1000, 200, port_num);
 movePos([-21, 21, 19, 0], port_num, 1.0, 3);
+movePos([-21, 21, 20, 10], port_num, 1.0, 3);
 pause(2); % tip
 movePos([-19, 19, 21, -30], port_num, 1.0, 3);
 movePos([-15, 15, 20, -30], port_num, 1.0, 3);
+movePos([-4, 4, 24, -30], port_num, 1.0, 3);
 
 %% return box
 setVAProfile(2000, 200, port_num);
+movePos([14, 0, 24, -30], port_num, 1.0, 3);
 movePos([18, 0, 18, -30], port_num, 2.0, 3);
 setVAProfile(1000, 200, port_num);
 movePos([18, 0, 14, -70], port_num, 1.0, 3)
@@ -59,22 +64,38 @@ movePos([-18, 0, 15, 0], port_num, 1, 3);
 movePos([-14.8, 14.8, 17, -3], port_num, 1.0, 3);
 
 %%
-movePos(egg_motion(1, 1:4), port_num, 1, 3);
-setVAProfile(200, 20, port_num);
-for i = 1:length(egg_motion)
-    movePos([egg_motion(i,1:3), (egg_motion(i,4)+(8*i/60))], port_num, 0.2, 3);
+numberOfCracks = 2;
+jugHeight = 15;
+for i = 1:numberOfCracks
+    movePos([-19, 23, jugHeight+5, 10], port_num, 1, 3);
+    setVAProfile(200, 100, port_num);
+    movePos([-19, 23, jugHeight, -10], port_num, 1, 3);
+    setVAProfile(1000, 200, port_num);
 end
-setVAProfile(1000, 200, port_num);
+% Pull back to open egg halves then raise again
+movePos([-15.1, 15.1, jugHeight, 0], port_num, 1, 3);
+movePos([-15.1, 15.1, jugHeight+5, 0], port_num, 1, 3);
 
-%%
+% Recorded egg motion; if doesn't work, use manual egg motion
+% movePos(egg_motion(1, 1:4), port_num, 1, 3);
+% setVAProfile(200, 20, port_num);
+% for i = 1:length(egg_motion)
+%     movePos([egg_motion(i,1:3), (egg_motion(i,4)+3)], port_num, 0.2, 3);
+% end
+% setVAProfile(1000, 200, port_num);
+
+%% drop egg
+movePos([-20, 0, jugHeight+5, 0], port_num, 1, 3);
 movePos([-20, 0, 15, -20], port_num, 1.0, 3);
+movePos([-18, 0, 10, -55], port_num, 1.0, 3); % egg drop
 openGripper(2, 1, port_num);
+movePos([-15, 0, working_height, -5], port_num, 1.0, 3);
 
 %% pick up spatula + bring to jug
 setVAProfile(2000, 200, port_num);
-movePos([15.8, 17, 18, -5], port_num, 2, 3); % swing round to above spatula - needs to be at working height
+movePos([15.8, 17, working_height, -5], port_num, 2, 3); % swing round to above spatula - needs to be at working height
 setVAProfile(1000, 200, port_num);
-movePos([15.8, 17, 6.5, -5], port_num, 1.0, 3); 
+movePos([15.8, 17, 7, 0], port_num, 1.0, 3); 
 openGripper(3, 0, port_num);
 pause(1);
 movePos([14, 15, 10, 0], port_num, 1.0, 3) % pick up spatula
@@ -89,44 +110,48 @@ movePos([-17.7, 17.8, 22, 0], port_num, 2.0, 3);
 movePos([-12, 12, 15, -30], port_num, 1.0, 3);
 
 movePos([-14.5, 14.5, 14, -60], port_num, 1.0, 3);
-setVAProfile(10,5,port_num);
-moveArc(-16, 16, 16, -60, 1.5, 2*pi, -4*pi, 180, port_num, 0.01, 3);
+movePos([-13, 14.5, 14, -60], port_num, 1.0, 3);
+%setVAProfile(200,100,port_num);
+setVAProfile(100,500,port_num); %uncomment prev line once we're sure it's safe
+moveArc(-14.5, 14.5, 14, -60, 1.5, 2*pi, -4*pi, 180, port_num, 0.1, 3);
 
 setVAProfile(1000,100,port_num);
 
 %% 
 % move spatula out of jug
 movePos([-12, 12, 15, -30], port_num, 1.0, 3);
-movePos([-17.7, 7.8, 17, 0], port_num, 1.0, 3);
+movePos([-17.7, 17.8, 17, 0], port_num, 1.0, 3);
 % drop off spatula
 movePos([14, 15, 10, 0], port_num, 1.0, 3)
-movePos([15.8, 17, 8, -5], port_num, 1.0, 3); 
-movePos([15.8, 17, 7, -5], port_num, 1.0, 3);
+movePos([15.8, 17, 10, 0], port_num, 1.0, 3); 
+movePos([15.8, 17, 7, 0], port_num, 1.0, 3);
 openGripper(2, 1, port_num);
-movePos([12.8, 14, 8, -5], port_num, 1.0, 3);
+pause(0.5);
+setVAProfile(500,100,port_num);
+movePos([12.8, 14, 8, 0], port_num, 1.0, 3);
+setVAProfile(1000,100,port_num);
 movePos([12.8, 14, 16, 0], port_num, 1.0, 3);
 
 
 %% Step 3: Pour batter into pan
 % % pick up jug 
 movePos([-13.7, 14, 16, -6], port_num, 2, 3); % move to above jug handle
-movePos([-13.7, 14, 7.1, -6], port_num, 1.0, 3);
+movePos([-13.7, 14, 8, 0], port_num, 1.0, 3);
 openGripper(3, 0, port_num);
-pause(1);
+pause(0.7);
 
 %%
 % pour jug contents
 movePos([-13.7, 14, 16, -6], port_num, 2, 3);
 movePos([0, 14, 22, -6], port_num, 2, 3);
 movePos([0, 14, 20, -50], port_num, 2, 3);
+pause(1.5);
 % return jug
-
-pause(2);
-
+movePos([0, 14, 22, -6], port_num, 2, 3);
 movePos([-13.7, 14, 20, -6], port_num, 2, 3);
-movePos([-13.7, 14, 7.1, -6], port_num, 1.0, 3);
+movePos([-13.7, 14, 8, 0], port_num, 1.0, 3);
 openGripper(3,1,port_num);
-
+pause(0.5);
 movePos([-13.7, 14, 16, -6], port_num, 2, 3);
 
 
@@ -134,9 +159,9 @@ movePos([-13.7, 14, 16, -6], port_num, 2, 3);
 
 setVAProfile(1000, 200, port_num);
 openGripper(3, 1, port_num);
-movePos([0, 20, 10, -8], port_num, 1, 3);
-movePos([0, 20, 3.8, -8], port_num, 1, 3);
-openGripper(3, 0, port_num);
+movePos([0, 20, 14, -8], port_num, 1, 3);
+movePos([0, 20, 3.8, 0], port_num, 1, 3);
+openGripper(3, 0, port_num); %now holding pan
 
 %%
 
@@ -153,8 +178,15 @@ setVAProfile(1000, 200, port_num);
 % Step 6: Serve - move off stove
 pause(2);
 movePos(flip_motion(1, 1:4), port_num, 1, 3);
-movePos([flip_motion(1, (1:2)), flip_motion(1, 3)+5, -45], port_num, 1, 3);
+movePos([0, 30, 12, -30], port_num, 1, 3);
 pause(1)
+
+% Put pan down
 movePos(flip_motion(1, 1:4), port_num, 1, 3);
+movePos([0, 20, 8, -8], port_num, 1, 3);
+movePos([0, 20, 3.8, 0], port_num, 1, 3);
+openGripper(3, 1, port_num);
+pause(0.5);
+movePos([0, 20, 8, 0], port_num, 1, 3);
 
 end
